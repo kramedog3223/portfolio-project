874718418de0a9148aff630e2c13f73284e1a51c
nick_k <nakramer1992@gmail.com>
2023-03-28 12:07:37 -0500

    updated scroll feature to fade away as scrolling up

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 function scrollAppear() {
   var aboutInfo = document.querySelector(".about-info");
   var aboutPosition = aboutInfo.getBoundingClientRect().top;
-  var screenPosition = window.innerHeight / 1.3;
+  var screenPosition = window.innerHeight / 1.5;
 
   if (aboutPosition < screenPosition) {
     aboutInfo.classList.add("about-appear");
+  } else {
+    aboutInfo.classList.remove("about-appear");
   }
 }
 
