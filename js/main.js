b052c742a40dcb240ea4724d3e46747d80798286
nick_k <nakramer1992@gmail.com>
2023-04-04 10:44:05 -0500

    updated header h2 with type writer effect

--- js/main.js
+++ js/main.js
@@ -1,5 +1,54 @@
 // header
+const textDisplay = document.querySelector(".intro-text");
+const phrases = [
+  " Front-End Web Developer ",
+  "Gamer",
+  " Dog Dad ",
+  " BBQ Enthusiast ",
+];
+let i = 0;
+let j = 0;
+let currentPhrase = [];
+let isDeleting = false;
+let isEnd = false;
 
+function loop() {
+  isEnd = false;
+  textDisplay.innerHTML = currentPhrase.join("");
+
+  if (i < phrases.length) {
+    if (!isDeleting && j <= phrases[i].length) {
+      currentPhrase.push(phrases[i][j]);
+      j++;
+      textDisplay.innerHTML = currentPhrase.join("");
+    }
+    if (isDeleting && j <= phrases[i].length) {
+      currentPhrase.pop(phrases[i][j]);
+      j--;
+      textDisplay.innerHTML = currentPhrase.join("");
+    }
+
+    if (j == phrases[i].length) {
+      isEnd = true;
+      isDeleting = true;
+    }
+
+    if (isDeleting && j === 0) {
+      currentPhrase = [];
+      isDeleting = false;
+      i++;
+      if (i == phrases.length) {
+        i = 0;
+      }
+    }
+  }
+
+  const spedUp = Math.random() * (80 - 50) + 50;
+  const normalSpeed = Math.random() * (300 - 200) + 200;
+  const time = isEnd ? 2000 : isDeleting ? spedUp : normalSpeed;
+  setTimeout(loop, time);
+}
+loop();
 // about section
 function scrollAppear() {
   var aboutInfo = document.querySelector(".about-info");
